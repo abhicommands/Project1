2dfdac430cf71d11832f99099db7c919622e9808
AkshithD <akshithd123@gmail.com>
2023-02-23 15:10:14 -0500

    memgrind changes

--- memgrind.c
+++ memgrind.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 #include "mymalloc.h"
 
 int main() {
+    // Test case 1: malloc() reserves unallocated memory
+    void *ptr1 = malloc(100);
+    void *ptr2 = malloc(200);
+    void *ptr3 = malloc(300);
+    void *ptr4 = malloc(400);
+    void *ptr5 = malloc(500);
+    void *ptr6 = malloc(600);
+    if (ptr1 == NULL || ptr2 == NULL || ptr3 == NULL || ptr4 == NULL || ptr5 == NULL || ptr6 == NULL) {
+        printf("Test case 1 failed: malloc() failed to reserve unallocated memory\n");
+    }
+
+    // Test case 2: malloc() returns a pointer to an object that does not overlap with any other allocated object
+    int *int_ptr = (int*) malloc(4);
+    char *char_ptr = (char*) malloc(100);
+    if (char_ptr <= (char*) int_ptr + 3) {
+        printf("Test case 2 failed: allocated objects overlap\n");
+    }
+
+    // Test case 3: writing to one object does not overwrite any other
+    char *str1 = (char*) malloc(1000);
+    char *str2 = (char*) malloc(2000);
+    memset(str1, '1', 1000);
+    memset(str2, '2', 2000);
+    for (int i = 0; i < 1000; i++) {
+        if (str1[i] != '1') {
+            printf("Test case 3 failed: object 1 has been overwritten\n");
+            break;
+        }
+    }
+    for (int i = 0; i < 2000; i++) {
+        if (i < 1000 && str2[i] != '2') {
+            printf("Test case 3 failed: object 2 has been overwritten before its start\n");
+            break;
+        } else if (i >= 1000 && str2[i] != '2') {
+            printf("Test case 3 failed: object 2 has been overwritten after its end\n");
+            break;
+        }
+    }
+
+    // Test case 4: free() deallocates memory
+    free(ptr1);
+    if (head->free == 0 || head->size != 100 + sizeof(Block)) {
+        printf("Test case 4 failed: memory was not deallocated correctly\n");
+    }
+
+    // Test case 5: adjacent free blocks are coalesced
+    free(ptr2);
+    if (head->free == 0 || head->size != 300 + sizeof(Block)) {
+        printf("Test case 5 failed: adjacent free blocks were not coalesced correctly\n");
+    }
+
     return 0;
 }
